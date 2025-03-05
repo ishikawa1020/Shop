28f07b3aa4fb7a8d8d45fab73bf015e7ac194bc8
ishikawa1020 <ishikawa.akihiko.961020@gmail.com>
2025-03-05 19:55:03 +0900

    [update]
    Co-Authored-By: megahoshi <185488843+megahoshi@users.noreply.github.com>

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const connectDB = async () => {
+const connectDB = async () => { 
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("mongodb connection success!");
