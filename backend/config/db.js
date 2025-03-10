eb30909905f70af490151c70cf686dd447f9d764
ishikawa1020 <ishikawa.akihiko.961020@gmail.com>
2025-03-10 16:10:27 +0900

    [update]

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const connectDB = async () => { 
+const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     console.log("mongodb connection success!");
   } catch (err) {
     console.log("mongodb connection failed!", err.message);
